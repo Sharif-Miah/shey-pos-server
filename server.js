40d017c5873464526fbb0cea8a4dcb949207264a
Sharif-Miah <farhansharif1410@gmail.com>
2025-08-26 19:49:20 +0600

    get All Items api route make

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const dbConnect = require('./dbConnect');
 const app = express();
+app.use(express.json());
+const itemsRoute = require('./routes/itemRoutes');
+
+app.use('/api/items/', itemsRoute);
 const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World! from Api.'));
