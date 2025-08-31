66b5cfab6a0baf67ec1c9a936e697ad081f3f519
Sharif-Miah <farhansharif1410@gmail.com>
2025-08-31 23:05:41 +0600

    edit route added

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -13,24 +13,27 @@ router.get('/get-all-items', async (req, res) => {
 
 router.post('/add-item', async (req, res) => {
   try {
-<<<<<<< HEAD
-    const newItem = new itemModel(req.body);
-=======
->>>>>>> 8bd7ff9e8e1725da97dfba545fd778c6bc10332c
     const value = {
       name: req.body.name,
       price: parseFloat(req.body.price),
       image: req.body.image,
       category: req.body.category,
     };
-<<<<<<< HEAD
-=======
     const newItem = new itemModel(value);
->>>>>>> 8bd7ff9e8e1725da97dfba545fd778c6bc10332c
+
     await newItem.save();
     res.status(200).json({ massage: 'Item added successfully' });
   } catch (error) {
     res.status(404).json(error);
+  }
+});
+
+router.post('/edit-item', async (req, res) => {
+  try {
+    await itemModel.findOneAndUpdate({ _id: req.body.itemId }, req.body);
+    res.send('Item Updated Successfully.');
+  } catch (error) {
+    res.status(500).json(error);
   }
 });
 
